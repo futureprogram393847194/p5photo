152f068c77468bd28b467769c22a8bfd16bb3337
futureprogram393847194 <127174671+futureprogram393847194@users.noreply.github.com>
2023-11-10 09:52:14 +0900

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let img;
+let cnv;
+function preload(){
+  img = loadImage(`assets/john.jpg`);
+}
+//only run once
+function setup() {
+  cnv = createCanvas(img.width, img.height);
+  //print(img.width img.height)
+  let newCanvasX = (windowWidth - img.width)/2;
+  let newCanvasY = (windowHeight - img .height)/2;
+  cnv.position(newCanvasX, newCanvasY);
+  //access the pixel information of the image
+  for (let col = 0; col< img.width; col+=2){
+    for (let row = 0; row <img.height; row+=2){
+    let xPos = col;
+    let yPos = row;
+    let c = img.get(xPos,yPos);
+    push();
+    translate(xPos, yPos);
+    rotate(radians(random(360)))
+    noFill();
+    stroke(color(c))
+    strokeWeight(random(5));
+    point(xPos,yPos);
+    strokeWeight(random(3));
+    //curve(x1, y1, x2, y2, x3, y3, x4, y4)
+    curve(xPos, yPos, sin(xPos) * random(60), cos(xPos) * sin(xPos) * random(90), random(10), random(80), cos(yPos) * sin(yPos) * random(140), cos(xPos) * sin(xPos) * 50)
+    pop();
+    } 
+  }
